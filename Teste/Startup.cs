923a2149aea0278861cf89057833809da87b6728
Igor Milione <ibruno.milione@gmail.com>
2019-03-18 07:46:07 -0300

    Adding scaffolded item DepartmentsController

--- Teste/Startup.cs
+++ Teste/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Teste.Models;
 
 namespace Teste
 {
@@ -22,6 +24,9 @@ namespace Teste
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<TesteContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("TesteContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
